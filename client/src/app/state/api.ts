36a3da83e3709e65352dedd9076a72666586783a
b-hexsoul <brandon.hexsel@gmail.com>
2024-09-28 16:04:31 -0700

    update rtk query with getDashboardMetrics action

--- client/src/app/state/api.ts
+++ client/src/app/state/api.ts
@@ -1,11 +1,17 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { IDashboardMetrics } from "@/app/interfaces/dashboard";
 
 // Define a service using a base URL and expected endpoints
 export const baseAPI = createApi({
   reducerPath: "baseAPI",
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
-  tagTypes: [],
-  endpoints: (builder) => ({}),
-})
+  tagTypes: ["DashboardMetrics"],
+  endpoints: (builder) => ({
+    getDashboardMetrics: builder.query<IDashboardMetrics, void>({
+      query: () => '/dashboard',
+      providesTags: ["DashboardMetrics"]
+    }),
+  }),
+});
 
-export const {} = baseAPI;
+export const { useGetDashboardMetricsQuery } = baseAPI;
