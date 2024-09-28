923da06de3c288eb896e1a2a622304caf4d71df6
b-hexsoul <brandon.hexsel@gmail.com>
2024-09-28 16:46:03 -0700

    add card popular products component

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -35,64 +35,64 @@ const Sidebar = () => {
   const dispatch = useAppDispatch();
 
   const sidebarClassnames = `fixed flex flex-col justify-between ${
-    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:-64"
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
   } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
 
   return (
     <div className={sidebarClassnames}>
-      <div>
-        <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${isSidebarCollapsed ? 'px-5' : 'px-8'}`}>
-          <div>Logo</div>
-          <h1 className={`${isSidebarCollapsed ? 'hidden' : 'block'} font-extrabold text-2xl`}>STOCKIT</h1>
-          <button
-            className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-            onClick={() => dispatch(toggleSidebar(!isSidebarCollapsed))}
-          >
-            <Menu className="w-4 h-4" />
-          </button>
-        </div>
-        <div className="flex-grow mt-8">
-          <nav>
-            <ul>
-              <li>
-                <SidebarLink
-                  href="/dashboard"
-                  icon={PanelsTopLeft}
-                  label="Dashboard"
-                />
-              </li>
-              <li>
-                <SidebarLink
-                  href="/inventory"
-                  icon={Archive}
-                  label="Inventory"
-                />
-              </li>
-              <li>
-                <SidebarLink
-                  href="/products"
-                  icon={Clipboard}
-                  label="Products"
-                />
-              </li>
-              <li>
-                <SidebarLink
-                  href="/settings"
-                  icon={Settings2}
-                  label="Settings"
-                />
-              </li>
-              <li>
-                <SidebarLink
-                  href="/expenses"
-                  icon={CircleDollarSign}
-                  label="Expenses"
-                />
-              </li>
-            </ul>
-          </nav>
-        </div>
+      <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${isSidebarCollapsed ? 'px-5' : 'px-8'}`}>
+        <div>Logo</div>
+        <h1 className={`${isSidebarCollapsed ? 'hidden' : 'block'} font-extrabold text-2xl`}>STOCKIT</h1>
+        <button
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => dispatch(toggleSidebar(!isSidebarCollapsed))}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
       </div>
+
+      <div className="flex-grow mt-8">
+        <nav>
+          <ul>
+            <li>
+              <SidebarLink
+                href="/dashboard"
+                icon={PanelsTopLeft}
+                label="Dashboard"
+              />
+            </li>
+            <li>
+              <SidebarLink
+                href="/inventory"
+                icon={Archive}
+                label="Inventory"
+              />
+            </li>
+            <li>
+              <SidebarLink
+                href="/products"
+                icon={Clipboard}
+                label="Products"
+              />
+            </li>
+            <li>
+              <SidebarLink
+                href="/settings"
+                icon={Settings2}
+                label="Settings"
+              />
+            </li>
+            <li>
+              <SidebarLink
+                href="/expenses"
+                icon={CircleDollarSign}
+                label="Expenses"
+              />
+            </li>
+          </ul>
+        </nav>
+      </div>
+
       <div className={`${isSidebarCollapsed ? 'hidden' : 'block'} mb-10`}>
         <p className="text-center text-xs text-gray-500">&copy; 2024 STOCKIT</p>
       </div>
