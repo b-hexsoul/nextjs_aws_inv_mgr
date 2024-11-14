e725f9eef403fef5bb872bf7ac66572d56669ad9
b-hexsoul <brandon.hexsel@gmail.com>
2024-11-13 17:04:06 -0800

    init aws amplify auth

--- server/prisma/migrations/20241114004152_init/migration.sql
+++ server/prisma/migrations/20241114004152_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "Users" (
     "userId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("userId")
 );
