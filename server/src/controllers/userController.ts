9b8da6937b353b67acb29b776310a7b186314110
b-hexsoul <brandon.hexsel@gmail.com>
2024-11-13 17:47:23 -0800

    update backend for aws auth

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -11,3 +11,42 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ message: "Error retrieving users" });
   }
 };
+
+export const getUser = async (req: Request, res: Response): Promise<void> => {
+  const { cognitoId } = req.params;
+  try {
+    const user = await prisma.users.findUnique({
+      where: {
+        cognitoId: cognitoId,
+      },
+    });
+
+    res.json(user);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving user: ${error.message}` });
+  }
+};
+
+export const postUser = async (req: Request, res: Response) => {
+  try {
+    const {
+      email,
+      cognitoId,
+      type,
+    } = req.body;
+    const newUser = await prisma.users.create({
+      data: {
+        email,
+        cognitoId,
+        type
+      },
+    });
+    res.json({ message: "User Created Successfully", newUser });
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving users: ${error.message}` });
+  }
+};
